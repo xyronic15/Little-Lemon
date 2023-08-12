6226842b266f88eda372656af828598ec52b1a3b
Xyron Brual <xyronb@gmail.com>
2023-08-12 12:05:59 -0400

    button changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ children }) => {
     return (
-        <div className='bg-[#F4CE14] w-fit h-fit font-bold py-5 px-12 rounded-[16px] text-[24px]'>
+        <div className='bg-[#F4CE14] hover:bg-[#333333] active:bg-[#495E57] hover:text-white active:text-white w-fit h-fit font-bold py-4 px-8 rounded-[16px] text-[24px]'>
             {children}
         </div>
     )
